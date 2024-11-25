6a6561cc1c159301694eb098564f6f1bfb79d30a
rubyddah <hardd2001@gmail.com>
2024-11-25 18:44:40 +0100

    fix conflict

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -17,3 +17,37 @@
     refs.modal.classList.toggle('is-open');
   }
 })();
+
+// SWITCH
+document.addEventListener('DOMContentLoaded', () => {
+  const themeToggles = [
+    document.getElementById('theme-toggle'),
+    document.getElementById('theme-toggle1'),
+  ];
+  const themeElements = document.querySelectorAll('[data-theme]');
+  const currentTheme = localStorage.getItem('theme') || 'light';
+
+  themeElements.forEach(element => {
+    element.setAttribute('data-theme', currentTheme);
+  });
+
+  themeToggles.forEach(toggle => {
+    toggle.checked = currentTheme === 'dark';
+  });
+
+  themeToggles.forEach(toggle => {
+    toggle.addEventListener('change', () => {
+      const newTheme = toggle.checked ? 'dark' : 'light';
+
+      themeElements.forEach(element => {
+        element.setAttribute('data-theme', newTheme);
+      });
+
+      themeToggles.forEach(otherToggle => {
+        otherToggle.checked = toggle.checked;
+      });
+
+      localStorage.setItem('theme', newTheme);
+    });
+  });
+});
